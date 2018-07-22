e8f81d521b4d818ab0ab2d49821d2a14a52fd47b
Ryan Benasutti <firey45@gmail.com>
2018-07-22 14:16:29 -0400

    Update example project with new artifact location

--- KernelExample/KernelExample.gradle.kts
+++ KernelExample/KernelExample.gradle.kts
@@ -6,10 +6,10 @@ description = "An example project using the publish kernel."
 
 repositories {
     maven {
-        setUrl("https://dl.bintray.com/octogonapus/okapi-ktor-test/")
+        setUrl("https://dl.bintray.com/octogonapus/maven-artifacts/")
     }
 }
 
 dependencies {
-    implementation(group = "org.okapilib", name = "Kernel", version = "0.0.2")
+    implementation(group = "org.okapilib", name = "kernel", version = "0.0.2")
 }
